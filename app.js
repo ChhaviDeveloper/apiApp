1a7d37e017220454ef603b722e3c86bcead0d860
Chhavi <103942483+ChhaviDeveloper@users.noreply.github.com>
2022-07-22 19:03:45 +0530

    updated

--- app.js
+++ app.js
@@ -28,6 +28,59 @@ app.get('/products',(req,res)=>{
     })
 })
 
+app.get('/top_pick',(req,res)=>{
+    db.collection('top_pick').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+app.get('/deal_of_day',(req,res)=>{
+    db.collection('deal_of_day').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+
+app.get('/women_category_price',(req,res)=>{
+    db.collection('women_category_price').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+app.get('/men_category',(req,res)=>{
+    db.collection('men_category').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+app.get('/kids_category',(req,res)=>{
+    db.collection('kids_category').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+
+app.get('/beauty_grooming',(req,res)=>{
+    db.collection('beauty_grooming').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+app.get('/categories_to_bag',(req,res)=>{
+    db.collection('categories_to_bag').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+
+    })
+})
+
+
 MongoClient.connect( mongoURL ,(err,client)=>{
     if(err) console.log("Error while Connecting");
     db = client.db('mongoDBApp');
